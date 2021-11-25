006a9bad087e63621e0d5b515021f33a3c046961
Minivera <gstpierre01@gmail.com>
2021-11-25 17:22:01 -0500

    Convertir le téléphone en CSS
    La version SVG avait trop de problème de performance et d'ajustement, cette version CSS s'intègre beaucoup mieux.

--- src/components/characterPortrait.tsx
+++ src/components/characterPortrait.tsx
@@ -1,9 +1,8 @@
 /* @jsx jsx */
 import { jsx, css } from '@emotion/react';
-import { FunctionComponent, useRef } from 'react';
+import { FunctionComponent } from 'react';
 
-import PortraitPhone from '../assets/ui/phone.svg?component';
-import PortraitPhoneWithBg from '../assets/ui/phone-with-background.svg?component';
+import { MobilePhone } from './mobilePhone';
 
 export interface CharacterPortraitProps {
   active?: boolean;
@@ -15,103 +14,42 @@ export const CharacterPortrait: FunctionComponent<CharacterPortraitProps> = ({
   children,
   inPhone,
   active,
-}) => {
-  const phoneRef = useRef<HTMLDivElement>(null);
+}) => (
+  <div
+    css={theme => css`
+      position: relative;
+      padding: 0 1.5rem;
+      max-height: 100%;
+      display: flex;
+      justify-content: center;
+      transition: transform 200ms ease-in-out;
+      transform-origin: center;
+      filter: drop-shadow(0 0 0.2rem ${theme.colors.yellow});
 
-  let positionFromTop = '0';
-  let positionFromLeft = '0';
-  let height = 'auto';
-  let width = 'auto';
-
-  if (phoneRef.current) {
-    const svg = phoneRef.current.querySelector('svg');
-    const g = phoneRef.current.querySelector('svg > g');
-
-    if (svg && g) {
-      height = g.getClientRects()[0].height.toFixed(2) + 'px';
-      width = g.getClientRects()[0].width.toFixed(2) + 'px';
-
-      positionFromTop =
-        (g.getClientRects()[0].y - svg.getClientRects()[0].y).toFixed(2) + 'px';
-      positionFromLeft =
-        (g.getClientRects()[0].x - svg.getClientRects()[0].x).toFixed(2) + 'px';
-    }
-  }
-
-  return (
-    <div
-      css={theme => css`
-        position: relative;
-        padding: 0 1.5rem;
-        max-height: 100%;
-        display: flex;
-        justify-content: center;
-        transition: transform 200ms ease-in-out;
-        transform-origin: center;
-        filter: drop-shadow(0 0 0.2rem ${theme.colors.yellow});
-
-        ${active
-          ? ''
-          : `
+      ${active
+        ? ''
+        : `
             transform: scale(0.95);
             filter: grayscale(40%);
           `}
-      `}
-    >
-      {inPhone ? (
-        <div
-          ref={phoneRef}
-          css={css`
-            display: grid;
-            height: 100%;
-            width: auto;
-            position: relative;
-            margin-bottom: -13rem;
-
-            & > div {
-              position: absolute;
-              width: auto;
-              overflow: hidden;
-              z-index: 1;
-              height: ${height};
-              border-radius: 70px;
-              top: ${positionFromTop};
-
-              & div {
-                padding-left: ${positionFromLeft};
-                padding-top: 5rem;
-              }
-
-              & svg {
-                width: ${width};
-              }
-            }
-          `}
-        >
-          <PortraitPhoneWithBg
-            css={theme => css`
-              height: 100%;
-              grid-row: 1;
-              grid-column: 1;
-
-              & rect {
-                fill: ${theme.colors.darkGray};
-              }
-            `}
-          />
-          {children}
-          <PortraitPhone
-            css={css`
-              height: 100%;
-              grid-row: 1;
-              grid-column: 1;
-              z-index: 2;
-            `}
-          />
-        </div>
-      ) : (
-        children
-      )}
-    </div>
-  );
-};
+    `}
+  >
+    {inPhone ? (
+      <MobilePhone
+        css={css`
+          align-self: flex-end;
+          margin-bottom: 10rem;
+        
+          & svg {
+            width: 200%;
+            margin-left: -50%;
+          }
+        `}
+      >
+        {children}
+      </MobilePhone>
+    ) : (
+      children
+    )}
+  </div>
+);
---
+++ src/components/mobilePhone.tsx
@@ -0,0 +1,54 @@
+/* @jsx jsx */
+import { jsx, css, Theme } from '@emotion/react';
+import { FunctionComponent } from 'react';
+import { Interpolation } from '@emotion/serialize';
+
+export const MobilePhone: FunctionComponent<{
+  css?: Interpolation<Theme>;
+}> = ({ children, ...rest }) => (
+  <div
+    css={theme => css`
+      z-index: 1;
+      width: 17rem;
+      height: 37rem;
+      padding: 0 0.5rem;
+      border: 0.25rem solid ${theme.colors.black};
+      border-radius: 1rem;
+      display: flex;
+      flex-direction: column;
+      background-color: ${theme.colors.white};
+      box-shadow: 0.5rem 0.5rem rgba(0, 0, 0, 0.3);
+      transition: all 0.3s;
+    `}
+    {...rest}
+  >
+    <div
+      css={theme => css`
+        height: 0.25rem;
+        width: 4rem;
+        margin: 1rem auto;
+        border-radius: 999rem;
+        background-color: ${theme.colors.darkGray};
+      `}
+    />
+    <div
+      css={theme => css`
+        flex: 1;
+        background-color: ${theme.colors.darkGray};
+        overflow: hidden;
+        padding: 3rem 1rem 0 1rem;
+      `}
+    >
+      {children}
+    </div>
+    <div
+      css={theme => css`
+        width: 1.5rem;
+        height: 1.5rem;
+        border: 2px solid ${theme.colors.darkGray};
+        border-radius: 50%;
+        margin: 1rem auto;
+      `}
+    />
+  </div>
+);
